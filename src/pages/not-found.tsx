4a35bbe7f82ee50403c1ef4607b11fd8d9a10f2e
Gabriel Agustin Uriarte <gabri02.uri@gmail.com>
2025-11-27 22:40:59 -0400

    chore: converted not-found image to webp

--- src/pages/not-found.tsx
+++ src/pages/not-found.tsx
@@ -6,7 +6,7 @@ export const NotFound: FC<NotFoundProps> = ({}) => {
 	return (
 		<section className="flex flex-1 h-full items-center justify-center">
 			<div className="flex flex-col gap-3 items-center justify-center">
-				<img src="/dafuq.png" className="h-80 w-70" alt="" />
+				<img src="/dafuq.webp" className="h-80 w-70" alt="" />
 				<span className="font-bold">Page not found</span>
 			</div>
 		</section>
